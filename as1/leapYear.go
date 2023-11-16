b4c2b66e17fdd77135d09fbbc1c40043a53729aa
Ahmed Khan <ahmed.khan@smallcase.com>
2023-11-16 20:13:36 +0000

    leap comment

--- as1/leapYear.go
+++ as1/leapYear.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+/**
+given a year N tell if it is leap year or not
+*/
+
 func isLeapYear(year int) bool {
 	// A year is a leap year if it is divisible by 4
 	// but years divisible by 100 are not leap years unless they are also divisible by 400
@@ -9,11 +13,8 @@ func isLeapYear(year int) bool {
 }
 
 func mainLeapYear() {
-	var year int
-	fmt.Print("Enter a year to check if it's a leap year: ")
-	fmt.Scan(&year)
 
-	if isLeapYear(year) {
+	if isLeapYear(2000) {
 		fmt.Printf("Leap Year")
 	} else {
 		fmt.Printf("Not Leap Year")
