f3870e8f7b91491250a0c9276b939bf9855cc197
Ahmed Khan <ahmed.khan@smallcase.com>
2023-11-27 15:58:24 +0000

    process input buffer

--- as0/processInput.go
+++ as0/processInput.go
@@ -10,18 +10,20 @@ import (
 
 func mainProcessInput() {
 	scanner := bufio.NewScanner(os.Stdin)
+	buf := make([]byte, 0, 64*1024)      // create a buffer with a 64KB initial size
+	scanner.Buffer(buf, 10*1024*1024)    // allow the buffer to grow to a maximum of 10MB
 	scanner.Scan()                       // Read the first line to get the number of test cases
 	t, _ := strconv.Atoi(scanner.Text()) // Convert the number of test cases to an integer
 
 	for i := 0; i < t; i++ {
 		scanner.Scan()                       // Read the line containing N and K
 		nk := strings.Fields(scanner.Text()) // Split the line into fields
-		n, _ := strconv.Atoi(nk[0])          // Convert N to an integer
-		k, _ := strconv.Atoi(nk[1])          // Convert K to an integer
+		N, _ := strconv.Atoi(nk[0])          // Convert N to an integer
+		K, _ := strconv.Atoi(nk[1])          // Convert K to an integer
 
 		scanner.Scan()                           // Read the line containing the array elements
 		arrStr := strings.Fields(scanner.Text()) // Split the line into fields
-		arr := make([]int, n)
+		arr := make([]int, N)
 		for j, s := range arrStr {
 			arr[j], _ = strconv.Atoi(s) // Convert each array element to an integer
 		}
@@ -30,7 +32,7 @@ func mainProcessInput() {
 		// You can now call a function to check for a subarray with sum k
 		// For example: hasSubarrayWithSum(arr, k)
 
-		fmt.Println("Read array:", arr, "with sum:", k)
+		fmt.Println("Read array:", arr, "with sum:", K)
 	}
 }
 
