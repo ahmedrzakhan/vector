bf7dacbba71e4358cc996908d36302b04ccfc73e
Ahmed Khan <ahmed.khan@smallcase.com>
2023-12-07 21:11:41 +0000

    set bits

---
+++ as3/setBits.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+/**
+Given an integer output the count of Set Bits present in it's binary representation.
+*/
+
+// countSetBits returns the count of set bits in the binary representation of n.
+func countSetBits(n int) int {
+	count := 0
+	for n > 0 {
+		count += n & 1 // Increment count if the least significant bit is 1
+		n >>= 1        // Shift n to the right by one to check the next bit
+	}
+	return count
+}
+
+func mainNum() {
+	input := `4
+1
+3
+7
+6`
+
+	reader := strings.NewReader(input)
+	scanner := bufio.NewScanner(reader)
+	scanner.Scan()
+	T, _ := strconv.Atoi(scanner.Text())
+
+	for i := 0; i < T; i++ {
+		scanner.Scan()
+		N, _ := strconv.Atoi(scanner.Text())
+		fmt.Println(countSetBits(N))
+	}
+}
