10f1b378eafa299a3587788c03ad1031ca38cb44
Ahmed Khan <ahmed.khan@smallcase.com>
2023-12-07 20:01:21 +0000

    complement

--- as2/complement.go
+++ as2/complement.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
-func complement(n int) int {
-	mask := 7
-	return (^n) & mask
+func findComplement(num int) int {
+	// To find complement, we need a mask which has all 1's in binary representation
+	// with the same length as num.
+	mask := ^0 // All 1's in binary.
+	for mask&num > 0 {
+		mask <<= 1
+	}
+	// XOR with mask flips the bits, as 1^1=0 and 0^1=1.
+	return ^mask & ^num
 }
 
-func mainComplement() {
-	n := 5
-	fmt.Println(complement(n))
+func main() {
+	input := `3
+5
+1
+2147483648`
+	reader := strings.NewReader(input)
+	scanner := bufio.NewScanner(reader)
+	// scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	t, _ := strconv.Atoi(scanner.Text())
+
+	for i := 0; i < t; i++ {
+		scanner.Scan()
+		n, _ := strconv.Atoi(scanner.Text())
+		complement := findComplement(n)
+		fmt.Println(complement)
+	}
 }
